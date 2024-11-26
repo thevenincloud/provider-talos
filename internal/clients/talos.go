7b90d5fcaa37bcd7d4dc572fcd9788d527d9f06f
Gabriel Olivieri <gabrieloandco@thevenin.io>
2024-11-26 17:19:29 +0100

    feat: custom

--- internal/clients/talos.go
+++ internal/clients/talos.go
@@ -39,28 +39,28 @@ func TerraformSetupBuilder(version, providerSource, providerVersion string) terr
 			},
 		}
 
-		configRef := mg.GetProviderConfigReference()
-		if configRef == nil {
-			return ps, errors.New(errNoProviderConfig)
-		}
-		pc := &v1beta1.ProviderConfig{}
-		if err := client.Get(ctx, types.NamespacedName{Name: configRef.Name}, pc); err != nil {
-			return ps, errors.Wrap(err, errGetProviderConfig)
-		}
+		// configRef := mg.GetProviderConfigReference()
+		// if configRef == nil {
+		// 	return ps, errors.New(errNoProviderConfig)
+		// }
+		// pc := &v1beta1.ProviderConfig{}
+		// if err := client.Get(ctx, types.NamespacedName{Name: configRef.Name}, pc); err != nil {
+		// 	return ps, errors.Wrap(err, errGetProviderConfig)
+		// }
 
-		t := resource.NewProviderConfigUsageTracker(client, &v1beta1.ProviderConfigUsage{})
-		if err := t.Track(ctx, mg); err != nil {
-			return ps, errors.Wrap(err, errTrackUsage)
-		}
+		// t := resource.NewProviderConfigUsageTracker(client, &v1beta1.ProviderConfigUsage{})
+		// if err := t.Track(ctx, mg); err != nil {
+		// 	return ps, errors.Wrap(err, errTrackUsage)
+		// }
 
-		data, err := resource.CommonCredentialExtractor(ctx, pc.Spec.Credentials.Source, client, pc.Spec.Credentials.CommonCredentialSelectors)
-		if err != nil {
-			return ps, errors.Wrap(err, errExtractCredentials)
-		}
-		creds := map[string]string{}
-		if err := json.Unmarshal(data, &creds); err != nil {
-			return ps, errors.Wrap(err, errUnmarshalCredentials)
-		}
+		// data, err := resource.CommonCredentialExtractor(ctx, pc.Spec.Credentials.Source, client, pc.Spec.Credentials.CommonCredentialSelectors)
+		// if err != nil {
+		// 	return ps, errors.Wrap(err, errExtractCredentials)
+		// }
+		// creds := map[string]string{}
+		// if err := json.Unmarshal(data, &creds); err != nil {
+		// 	return ps, errors.Wrap(err, errUnmarshalCredentials)
+		// }
 
 		// Set credentials in Terraform provider configuration.
 		/*ps.Configuration = map[string]any{
